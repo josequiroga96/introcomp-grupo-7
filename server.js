29393711ec43dfad87a90fda6a33e5fe140ffc5b
JoseRojas96 <jose.rojas@ing.austral.edu.ar>
2020-08-19 18:04:51 -0300

    added server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 8080
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
