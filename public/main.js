659451161c61a978e5d168da5e27d340c0f12684
Joaquin Iannuzzi <joaco.iannuzzi@gmail.com>
2020-08-26 12:19:10 -0300

    separated js from html

---
+++ public/main.js
@@ -0,0 +1,39 @@
+window.addEventListener("devicemotion", handleMotion)
+function handleMotion(event) {
+  const output = document.getElementById("motion-output")
+  // console.log(event)
+
+  let properties = ""
+  for (let prop in event) {
+    if (prop === "acceleration") {
+      properties += prop + " x: " + event.acceleration.x + "<br>"
+      properties += prop + " y: " + event.acceleration.y + "<br>"
+      properties += prop + " z: " + event.acceleration.z + "<br>"
+    } else if (prop === "accelerationIncludingGravity") {
+      properties +=
+        prop + " x: " + event.accelerationIncludingGravity.x + "<br>"
+      properties +=
+        prop + " y: " + event.accelerationIncludingGravity.y + "<br>"
+      properties +=
+        prop + " z: " + event.accelerationIncludingGravity.z + "<br>"
+    } else if (prop === "rotationRate") {
+      properties += prop + " alpha: " + event.rotationRate.alpha + "<br>"
+      properties += prop + " beta: " + event.rotationRate.beta + "<br>"
+      properties += prop + " gamma: " + event.rotationRate.gamma + "<br>"
+    } else {
+      properties += prop + ": " + event[prop] + "<br>"
+    }
+  }
+  output.innerHTML = properties
+}
+
+window.addEventListener("deviceorientation", handleOrientation)
+function handleOrientation(event) {
+  const output = document.getElementById("orientation-output")
+  //   console.log(event)
+  let properties = ""
+  for (let prop in event) {
+    properties += prop + ": " + event[prop] + "<br>"
+  }
+  output.innerHTML = properties
+}
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-var cors = require('cors')
+const express = require("express")
+const cors = require("cors")
+
 const app = express()
 const port = 8080
 
-const path = require('path')
-
 app.use(cors())
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/sensors.html')))
+app.use(express.static(__dirname + "/public"))
 
-app.post('info', (req, res) => {
-    console.log(req.body)
+app.post("api/info", (req, res) => {
+  console.log(req.body)
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+)
