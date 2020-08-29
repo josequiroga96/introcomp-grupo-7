96d4548e9b364462f29f69e43ddda06c55aa5c58
joacoiannuzzi <joaco.iannuzzi@gmail.com>
2020-08-29 18:12:37 -0300

    didn't work

--- public/main.js
+++ public/main.js
@@ -10,41 +10,57 @@ async function postData(url = "", data = {}) {
     },
     body: JSON.stringify(data),
   })
-
-  // .then(() => console.log("Send Data"))
-  // return response.json()
 }
 
 postData(url, { message: "init" })
 
 const output = document.getElementById("motion-output")
 
-const deviceMotionFun = (event) => {
+window.addEventListener('devicemotion', onDeviceMotion)
 
-  const { acceleration, accelerationIncludingGravity, rotationRate } = event
-  const data = { acceleration, accelerationIncludingGravity, rotationRate }
+function onDeviceMotion(event) {
+
+  const {
+    acceleration: {
+      x: accX,
+      y: accY,
+      z: accz
+    },
+    accelerationIncludingGravity: {
+      x: accGrX,
+      y: accGrY,
+      z: accGrZ
+    },
+    rotationRate: {
+      alpha,
+      beta,
+      gamma
+    }
+  } = event
+
+  const data = {
+    acceleration: {
+      x: accX,
+      y: accY,
+      z: accz
+    },
+    accelerationIncludingGravity: {
+      x: accGrX,
+      y: accGrY,
+      z: accGrZ
+    },
+    rotationRate: {
+      alpha,
+      beta,
+      gamma
+    }
+  }
+
+  console.log(data)
 
   output.innerHTML = JSON.stringify(data)
 
   postData(url, data)
 }
 
-window.ondevicemotion = deviceMotionFun
-
-
-// document.getElementById('allow-sensors').onclick = () => {
-//   // feature detect
-//   // if (typeof DeviceMotionEvent.requestPermission === 'function') {
-//   DeviceMotionEvent.requestPermission()
-//     .then(permissionState => {
-//       if (permissionState === 'granted') {
-//         window.addEventListener('devicemotion', deviceMotionFun);
-//       }
-//     })
-//     .catch(console.error);
-//   // } else {
-//   //   // handle regular non iOS 13+ devices
-//   //   window.addEventListener('devicemotion', deviceMotionFun);
-//   // }
-// }
 
