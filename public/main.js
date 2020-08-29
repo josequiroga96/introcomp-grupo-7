9b252a24ee04f63362ec5fdfd68107a3f7172005
joacoiannuzzi <joaco.iannuzzi@gmail.com>
2020-08-29 15:10:17 -0300

    trying stuff

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 // http://192.168.200.39:8080/api/info
 
-const url = "http://localhost:8080/api/info"
+const url = "http://192.168.1.100:8080/api/info"
 
 async function postData(url = "", data = {}) {
   fetch(url, {
@@ -17,55 +17,34 @@ async function postData(url = "", data = {}) {
 
 postData(url, { message: "init" })
 
-window.addEventListener("devicemotion", (event) => {
-  const output = document.getElementById("motion-output")
+const output = document.getElementById("motion-output")
 
-  let properties = ""
-  let data = {}
-  for (let prop in event) {
-    if (prop === "acceleration") {
-        data[prop] = {
-            x: event[prop].x,
-            y: event[prop].y,
-            z: event[prop].z
-        }
-        properties += prop + " x: " + event.acceleration.x + "<br>"
-        properties += prop + " y: " + event.acceleration.y + "<br>"
-        properties += prop + " z: " + event.acceleration.z + "<br>"
-    } else if (prop === "accelerationIncludingGravity") {
-        data[prop] = {
-            x: event[prop].x,
-            y: event[prop].y,
-            z: event[prop].z
-        }
-        properties += prop + " x: " + event.accelerationIncludingGravity.x + "<br>"
-        properties += prop + " y: " + event.accelerationIncludingGravity.y + "<br>"
-        properties += prop + " z: " + event.accelerationIncludingGravity.z + "<br>"
-    } else if (prop === "rotationRate") {
-        data[prop] = {
-            alpha: event[prop].alpha,
-            beta: event[prop].beta,
-            gamma: event[prop].gamma
-        }
-        properties += prop + " alpha: " + event.rotationRate.alpha + "<br>"
-        properties += prop + " beta: " + event.rotationRate.beta + "<br>"
-        properties += prop + " gamma: " + event.rotationRate.gamma + "<br>"
-    } else {
-        // data[prop] = event[prop]
-        properties += prop + ": " + event[prop] + "<br>"
-    }
-  }
-  output.innerHTML = properties
+const deviceMotionFun = (event) => {
 
-  postData(url, { data: data })
-})
+  const { acceleration, accelerationIncludingGravity, rotationRate } = event
+  const data = { acceleration, accelerationIncludingGravity, rotationRate }
 
-window.addEventListener("deviceorientation", handleOrientation)
-function handleOrientation(event) {
-  const output = document.getElementById("orientation-output")
-  let properties = ""
-  for (let prop in event) {
-    properties += prop + ": " + event[prop] + "<br>"
-  }
-  output.innerHTML = properties
+  output.innerHTML = JSON.stringify(data)
+
+  postData(url, data)
 }
+
+window.ondevicemotion = deviceMotionFun
+
+
+// document.getElementById('allow-sensors').onclick = () => {
+//   // feature detect
+//   // if (typeof DeviceMotionEvent.requestPermission === 'function') {
+//   DeviceMotionEvent.requestPermission()
+//     .then(permissionState => {
+//       if (permissionState === 'granted') {
+//         window.addEventListener('devicemotion', deviceMotionFun);
+//       }
+//     })
+//     .catch(console.error);
+//   // } else {
+//   //   // handle regular non iOS 13+ devices
+//   //   window.addEventListener('devicemotion', deviceMotionFun);
+//   // }
+// }
+
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 8080
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 const api = express.Router()
 
